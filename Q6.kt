99649e0b7557a64b53aae7eb17dd975805eb8314
a10033135 <49735907+a10033135@users.noreply.github.com>
2019-06-15 12:46:16 +0800

    Update Q6.kt

--- Q6.kt
+++ Q6.kt
@@ -2,20 +2,18 @@ open class Human(val name: String? = null){
     val mana : Any? =null
     open fun attack(  ) {
         val Human = Human("Fist Attack!")
-        println(Human.name) }
-}
-
-open class mage () :Human(null){
-
-    override fun attack() {
-        val Human = Human("Fire Ball!")
-        println(Human.name)
-
+        println(Human.name) 
     }
 }
 
-fun main()
-{
+open class mage () :Human(null){
+    override fun attack() {
+        val Human = Human("Fire Ball!")
+        println(Human.name)
+    }
+}
+
+fun main(){
     val flag = Human().mana
     val obj = mage()
     obj.attack()
@@ -23,5 +21,4 @@ fun main()
         null -> println("I have no mana")
         else -> println("I have mana")
     }
-
 }
